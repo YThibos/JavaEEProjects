96de0fb88b60ebed0dc6db9ad8a0242f3320f7a5
Yannick Thibos <yannick.thibos@gmail.com>
2016-02-17 12:25:09 +0100

    2nd try [push from C:\workspace]

--- pizzaluigi/src/be/vdab/servlets/IndexServlet.java
+++ pizzaluigi/src/be/vdab/servlets/IndexServlet.java
@@ -15,7 +15,9 @@ import be.vdab.entities.Begroeting;
  * Servlet implementation class IndexServlet
  */
 @WebServlet("/index.htm")
-public class IndexServlet extends HttpServlet {
+public class IndexServlet extends HttpServlet  {
+	
+	
 	
 	private static final long serialVersionUID = 1L;
 	private static final String VIEW = "/WEB-INF/JSP/index.jsp";
@@ -27,5 +29,11 @@ public class IndexServlet extends HttpServlet {
 		request.getRequestDispatcher(VIEW).forward(request, response);
 		
 	}
-
+	
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doPost(req, resp);
+	}
 }
